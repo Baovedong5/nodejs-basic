fc224603885e7c173051cf87a7b04900ccde9e66
Phuong <lephuong10142002@gmail.com>
2023-05-18 15:06:39 +0700

    using view engine

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express(); // app express
 const port = 8080; // declare prot
+
+//config template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //khai bao route
 app.get("/", (req, res) => {
@@ -12,7 +17,8 @@ app.get("/abc", (req, res) => {
 });
 
 app.get("/test", (req, res) => {
-  res.send("<h1>PhuongDZ</h1>");
+  // res.send("<h1>PhuongDZ</h1>");
+  res.render("exsample.ejs");
 });
 
 app.listen(port, () => {
